c7018167d554f4842ccf02bd31482c2106e5a3ab
nickwallner <nickwallner@outlook.com>
2025-09-28 20:25:05 -0600

    remove irrelevant section of code

--- blinky_systick/src/main.c
+++ blinky_systick/src/main.c
@@ -6,10 +6,6 @@ int main(void)
     the system clock for the GPIOA peripheral */
     RCC->AHB1ENR |= BIT(0);
 
-    /* set bit 14 in the APB2ENR register to enable
-    the system clock for the SYSCFG peripheral */
-    RCC->APB2ENR |= BIT(14);
-
     /* LD2 is connected to PA5, so set PA5 to output mode */
     GPIO_Set_Mode(GPIOA, PIN5, GPIO_MODE_OUTPUT);
 
