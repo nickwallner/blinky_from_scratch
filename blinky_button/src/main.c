c098c2d9f454f1b4391056b4d2bb3787c8bf1444
nickwallner <nickwallner@outlook.com>
2025-10-01 15:16:43 -0600

    add 2 new blinking methods using pushbutton, improve code readability/useability

--- blinky_button/src/main.c
+++ blinky_button/src/main.c
@@ -1,7 +1,16 @@
 #include "include/main.h"
 
+/* choose which blinking method to use, only one
+define should be uncommented at a time! */
+#define HOLD_CONSTANT
+//#define TOGGLE_CONSTANT
+//#define HOLD_BLINKING
+//#define TOGGLE_BLINKING
+
 int main(void)
 {
+    __attribute__((unused)) uint8_t toggled_flag = 0; // flag for toggling led blinking
+
     /* set bit 0 and 2 in the AHB1ENR register to enable 
     the system clock for the GPIOA and GPIOC peripherals */
     RCC->AHB1ENR |= (BIT(0) | BIT(2));
@@ -20,6 +29,22 @@ int main(void)
         hardware debouncing, so we don't need to worry about
         debouncing the button input with software */
 
+        #ifdef HOLD_CONSTANT
+
+        /* ------------------------------------------------ */
+        /* hold the button to toggle the led to constant on */
+        /* ------------------------------------------------ */
+
+        while (GPIO_Read(GPIOC, PIN13) == GPIO_PIN_RESET)
+        {
+            GPIO_Write(GPIOA, PIN5, GPIO_PIN_SET);
+        }
+
+        GPIO_Write(GPIOA, PIN5, GPIO_PIN_RESET);
+
+        #endif // HOLD_CONSTANT
+        #ifdef TOGGLE_CONSTANT
+
         /* ------------------------------------------------------ */
         /* use the button to toggle the led to constant on or off */
         /* ------------------------------------------------------ */
@@ -35,15 +60,52 @@ int main(void)
         so we don't repeatedly toggle the led */
         while (GPIO_Read(GPIOC, PIN13) == GPIO_PIN_RESET) {}
 
+        #endif // TOGGLE_CONSTANT
+        #ifdef HOLD_BLINKING
+
         /* --------------------------------------------- */
         /* hold the button to stop the led from blinking */
         /* --------------------------------------------- */
 
         /* while button is pressed, turn the led off and wait for the button to be released */
-        //while (GPIO_Read(GPIOC, PIN13) == GPIO_PIN_RESET) { GPIO_Write(GPIOA, PIN5, GPIO_PIN_RESET); }
+        while (GPIO_Read(GPIOC, PIN13) == GPIO_PIN_RESET) { GPIO_Write(GPIOA, PIN5, GPIO_PIN_RESET); }
 
         /* blink the led on 0.5 second intervals when the button isnt pressed */
-        //GPIO_Toggle(GPIOA, PIN5);
-        //SYSTICK_Delay(500);
+        GPIO_Toggle(GPIOA, PIN5);
+        SYSTICK_Delay(500);
+
+        #endif // HOLD_BLINKING
+        #ifdef TOGGLE_BLINKING
+
+        /* ------------------------------------------*/
+        /* use the button to toggle the led blinking */
+        /* ------------------------------------------*/
+
+        /* this method of toggling the led is not perfect, you might press
+        the button but the led doesnt toggle. this is because the button
+        is only polled once per loop iteration, so if you press the button
+        after the button has been polled for a given iteration, nothing will
+        happen. it would be better to use an external interrupt for the 
+        button press to guarantee the led is toggled every button press */
+
+        /* poll the button to check if its pressed */
+        if (GPIO_Read(GPIOC, PIN13) == GPIO_PIN_RESET)
+        {
+            toggled_flag = !toggled_flag; // invert toggled flag
+            GPIO_Write(GPIOA, PIN5, GPIO_PIN_RESET); // turn off led
+
+            /* do nothing while the buttons remains pressed, to ensure
+            we don't repeatedly toggle the flag */
+            while (GPIO_Read(GPIOC, PIN13) == GPIO_PIN_RESET) {}
+        }
+
+        /* if toggled_flag is not set, blink the led */
+        if (toggled_flag == 0)
+        {
+            GPIO_Toggle(GPIOA, PIN5);
+            SYSTICK_Delay(500);
+        }
+
+        #endif // TOGGLE_BLINKING
     }
 }
